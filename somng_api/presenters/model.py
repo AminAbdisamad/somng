492755735df0e4c14918b956fd40815e95d126ef
Amin Abdisamad <amin.abdisamad@b4finance.com>
2021-06-23 02:54:46 +0300

    added database design :rocket

---
+++ somng_api/presenters/model.py
@@ -0,0 +1,4 @@
+from sqlalchemy.orm import Session, relationship
+from sqlalchemy import String, Column, Boolean, DateTime, Integer
+from database import Base
+from database.core import TimeStampMixin, ContactMixin
